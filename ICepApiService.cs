48be55ab005660ff0e958f3c0c386b7b02b90466
Kaio Alves <kaio.alves7@hotmail.com>
2020-02-22 00:09:33 -0300

    Criaçao do projeto

---
+++ ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Refit;
+
+namespace buscar_cep
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAdressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace buscar_cep
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+                Console.WriteLine("Digite o CEP:");
+
+                string cepDigitado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do CEP: " + cepDigitado);
+
+                var adress = await cepClient.GetAdressAsync(cepDigitado);
+
+                Console.WriteLine($"\nLogradouro: { adress.Logradouro} \nBairro: {adress.Bairro} \nCidade: {adress.Localidade}/{adress.UF}");
+                Console.ReadKey();
+            } catch (Exception e)
+            {
+                Console.WriteLine("Erro ao consultar CEP! Verifique o CEP digitado e tente novamente.");
+                //Console.WriteLine("Erro ao consultar CEP! Verifique o CEP digitado e tente novamente." + "\n" + e.Message);
+            }
+
+        }
+    }
+}
